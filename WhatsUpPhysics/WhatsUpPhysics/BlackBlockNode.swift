892cc8f2b62d59cd12af2e7aefd4395f5fb9b4c2
Zach Bebel <zlb3507@rit.edu>
2016-04-29 22:47:25 -0400

    Black Box = Cool beats

--- WhatsUpPhysics/WhatsUpPhysics/BlackBlockNode.swift
+++ WhatsUpPhysics/WhatsUpPhysics/BlackBlockNode.swift
@@ -14,6 +14,7 @@ class BlackBlockNode: SKSpriteNode {
         
         // Enlarge and then shrink
         runAction(SKAction.sequence([
+            SKAction.playSoundFileNamed("black block.mp3", waitForCompletion: false),
             SKAction.scaleBy(1.1, duration: 0.1),
             SKAction.scaleBy(0.9, duration: 0.1)
         ]))
